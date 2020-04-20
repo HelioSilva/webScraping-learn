443e61bb031b237d0eb265e50c00945565553682
Helio Silva <suporte.computek@gmail.com>
2020-04-20 01:01:44 -0300

    Config Prettier,eslint

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # WebScraping
 
 - The Repo initially created for web Scraping, but over time, interest arose in analyzing the Ifood website, to find the sale of ACAí.
+
+i really like açaí, i will do an automation to find discounted prices on iFood.
+I also want the app to send a message to me on WhatsApp with the discounted product.
--- index.js
+++ index.js
@@ -1,100 +1,92 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
+const run = async function (browser) {
+  const page = await browser.newPage();
+  page.setViewport({ width: 1024, height: 768 });
+  await page.goto("https://www.ifood.com.br/delivery/maceio-al/cozinha/acai");
+  //await page.screenshot({path:'exemple.png'})
+  //await page.pdf({path:'exemple.pdf',format:'A4'});
 
+  const request1 = await page.evaluate(() => {
+    const restaurantes = document.querySelectorAll("a.restaurant-card");
+    //const nomesRestaurantes = document.querySelectorAll('span.restaurant-name')
 
-const run = async function(browser){
+    const resultadoBusca = Array.from(restaurantes).map((restaurante) => {
+      return {
+        nome: restaurante.querySelector("span.restaurant-name").innerText,
+        imagem: restaurante.querySelector("img.restaurant-card__img-logo").src,
+        rating: restaurante.querySelector("span.restaurant-rating").innerText,
+        tempoEstimado: restaurante.querySelector("div.restaurant-card__footer")
+          .innerText,
+        uri: restaurante.href,
+      };
+    });
+    return resultadoBusca;
+  });
+
+  fs.writeFileSync("./dados.json", JSON.stringify(request1, null, 2));
+
+  return request1;
+};
+
+const leituraItem = async function lerItens(browser, dados) {
+  let registro = [];
+
+  //dados.length
+
+  for (let index = 0; index < dados.length; index++) {
+    const item = dados[index];
 
     const page = await browser.newPage();
-    page.setViewport({width:1024,height:768});    
-    await page.goto('https://www.ifood.com.br/delivery/maceio-al/cozinha/acai');
-    //await page.screenshot({path:'exemple.png'}) 
-    //await page.pdf({path:'exemple.pdf',format:'A4'});
+    page.setViewport({ width: 1024, height: 768 });
+    await page.goto(item.uri);
 
-    const request1 = await page.evaluate(()=>{
-        
-        const restaurantes = document.querySelectorAll('a.restaurant-card')
-        //const nomesRestaurantes = document.querySelectorAll('span.restaurant-name')
+    const requestItem = await page.evaluate(() => {
+      const prods = document.querySelectorAll("a.dish-card");
+      return Array.from(prods).map((prod) => {
+        let descricao =
+          prod.querySelector("span.dish-card__description") != null
+            ? prod.querySelector("span.dish-card__description").innerText
+            : "";
 
-        const resultadoBusca = Array.from(restaurantes).map((restaurante)=>{
-            return{
-                nome: restaurante.querySelector('span.restaurant-name').innerText,
-                imagem: restaurante.querySelector('img.restaurant-card__img-logo').src,
-                rating: restaurante.querySelector('span.restaurant-rating').innerText,
-                tempoEstimado: restaurante.querySelector('div.restaurant-card__footer').innerText,
-                uri:restaurante.href
-            } 
-        })
+        let vlorDesconto =
+          prod.querySelector("span.dish-card__price--discount") != null
+            ? prod.querySelector("span.dish-card__price--discount").firstChild
+                .textContent
+            : "";
 
-        return resultadoBusca;
-         
+        let vlorOriginal =
+          prod.querySelector("span.dish-card__price--original") != null
+            ? prod.querySelector("span.dish-card__price--original").innerText
+            : "";
+
+        return {
+          nomeProduto: descricao,
+          valorDesconto: vlorDesconto,
+          valorOriginal: vlorOriginal,
+        };
+      });
     });
 
-    fs.writeFileSync("./dados.json" , JSON.stringify( request1, null , 2 ) );
+    item["produtos"] = requestItem;
 
-    return request1 ;    
-}
+    registro.push(item);
 
-const leituraItem = async function lerItens(browser,dados){
+    await page.close();
+  }
 
-    let registro = [];
+  fs.writeFileSync("./precos.json", JSON.stringify(registro, null, 2));
+  return registro;
+};
 
-    
-        for (let index = 0; index < dados.length; index++) {
-            const item = dados[index];
+const main = async function () {
+  const browser = await puppeteer.launch({ headless: true });
 
-            const page = await browser.newPage();
-            page.setViewport({width:1024,height:768});  
-            await page.goto(item.uri);
-            
+  const estabelecimentos = await run(browser);
+  await leituraItem(browser, estabelecimentos);
 
-            const requestItem = await page.evaluate(()=>{
-                const prods = document.querySelectorAll('a.dish-card');
-                return Array.from(prods).map((prod)=>{
+  await browser.close();
+};
 
-                    let descricao = prod.querySelector('span.dish-card__description') != null ? 
-                                    prod.querySelector('span.dish-card__description').innerText : "";
-
-                    let vlorDesconto = prod.querySelector('span.dish-card__price--discount') != null ? 
-                                    prod.querySelector('span.dish-card__price--discount').innerText : "";
-
-                    let vlorOriginal = prod.querySelector('span.dish-card__price--original') != null ? 
-                                    prod.querySelector('span.dish-card__price--original').innerText : "";
-                    
-
-                    return{
-                        nomeProduto : descricao   ,  
-                        valorDesconto : vlorDesconto ,
-                        valorOriginal : vlorOriginal
-                    }
-                })
-            })
-
-            item["produtos"] = requestItem ;
-
-            registro.push(item);     
-           
-            await page.close()
-            
-        }
-
-    
-
-        fs.writeFileSync("./precos.json" , JSON.stringify( registro , null , 2 ) );
-        return  registro ;
-   }
-
-
-
-const main = async function(){     
-
-    const browser = await puppeteer.launch({headless:true});
-
-    const estabelecimentos = await run(browser);
-    const itens = await leituraItem(browser,estabelecimentos);
-
-    await browser.close()
-
-}
-
-main();    
+main();
