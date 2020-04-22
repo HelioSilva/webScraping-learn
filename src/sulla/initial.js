8b392e19f69e160518e461b815e015067bfa465e
Helio Silva <suporte.computek@gmail.com>
2020-04-22 15:11:21 -0300

    alter

--- src/sulla/initial.js
+++ src/sulla/initial.js
@@ -6,15 +6,15 @@ const start = async function start(client) {
   await schedule.scheduleJob(" */3 * * * * ", async function () {
     console.log("Inicio da execução do gatilho!");
     await client.sendText("558296130940@c.us", "Gatilho iniciado...");
-    const anyProds = await controlIFood.main();
+    await controlIFood.main();
 
-    for (let i = 0; i < anyProds.length; i++) {
-      const element = anyProds[i];
-      await client.sendText(
-        "558296130940@c.us",
-        `${element.nome} \n ${element.produtos[0].nomeProduto} \n Preço: ${element.produtos[0].valorDesconto}`
-      );
-    }
+    // for (let i = 0; i < 1; i++) {
+    //   const element = anyProds[i];
+    //   await client.sendText(
+    //     "558296130940@c.us",
+    //     `${element.nome} \n ${element.produtos[0].nomeProduto} \n Preço: ${element.produtos[0].valorDesconto}`
+    //   );
+    // }
 
     console.log("Fim da execução do gatilho!");
   });
